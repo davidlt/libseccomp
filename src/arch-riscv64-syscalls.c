2a70ad4f3e8ab80e88f0662a760f4ef1d9219205
David Abdurachmanov <david.abdurachmanov@gmail.com>
2018-12-06 12:06:38 +0100

    Add support for RISC-V RV64 (64-bit, riscv64)
    The patch adds support for riscv64 architecture. It also enables
    the following syscalls:
    - riscv_flush_icache (RISC-V RV32/RV64 specific)
    - preadv2
    - pwritev2
    - renameat
    - renameat2
    - io_pgetevents
    - rseq
    
    This was tested on Fedora 29/RISCV [0] using 4.19 kernel.
    Tested on top of libseccomp commit:
    1e64feb5f1a9ea02687228e3073e8b784a04ce46
    
    Results for testing:
    
    ./regression -T live
    
    Regression Test Summary
     tests run: 8
     tests skipped: 0
     tests passed: 8
     tests failed: 0
     tests errored: 0
    
    ./regression
    
    Regression Test Summary
     tests run: 5129
     tests skipped: 104
     tests passed: 5129
     tests failed: 0
     tests errored: 0
    
    [0] https://fedoraproject.org/wiki/Architectures/RISC-V
    
    Signed-off-by: David Abdurachmanov <david.abdurachmanov@gmail.com>

--- src/arch-riscv64-syscalls.c
+++ src/arch-riscv64-syscalls.c
@@ -1,8 +1,8 @@
 /**
- * Enhanced Seccomp AArch64 Syscall Table
+ * Enhanced Seccomp RISC-V 64-bit (riscv64) Syscall Table
  *
- * Copyright (c) 2014 Red Hat <mjuszkiewicz@redhat.com>
- * Author: Marcin Juszkiewicz <mjuszkiewicz@redhat.com>
+ * Copyright (c) 2018 David Abdurachmanov <david.abdurachmanov@gmail.com>
+ * Author: David Abdurachmanov <david.abdurachmanov@gmail.com>
  */
 
 /*
@@ -24,10 +24,10 @@
 #include <seccomp.h>
 
 #include "arch.h"
-#include "arch-aarch64.h"
+#include "arch-riscv64.h"
 
-/* NOTE: based on Linux 4.15-rc7 */
-const struct arch_syscall_def aarch64_syscall_table[] = { \
+/* NOTE: based on Linux 4.19-rc8 */
+const struct arch_syscall_def riscv64_syscall_table[] = { \
 	{ "_llseek", __PNR__llseek },
 	{ "_newselect", __PNR__newselect },
 	{ "_sysctl", __PNR__sysctl },
@@ -168,7 +168,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "io_cancel", 3 },
 	{ "io_destroy", 1 },
 	{ "io_getevents", 4 },
-	{ "io_pgetevents", __PNR_io_pgetevents },
+	{ "io_pgetevents", 292},
 	{ "io_setup", 0 },
 	{ "io_submit", 2 },
 	{ "ioctl", 29 },
@@ -298,7 +298,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "request_key", 218 },
 	{ "restart_syscall", 128 },
 	{ "rmdir", __PNR_rmdir },
-	{ "rseq", __PNR_rseq },
+	{ "rseq", 293 },
 	{ "rt_sigaction", 134 },
 	{ "rt_sigpending", 136 },
 	{ "rt_sigprocmask", 135 },
@@ -475,10 +475,10 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
  * numbers; returns __NR_SCMP_ERROR on failure.
  *
  */
-int aarch64_syscall_resolve_name(const char *name)
+int riscv64_syscall_resolve_name(const char *name)
 {
 	unsigned int iter;
-	const struct arch_syscall_def *table = aarch64_syscall_table;
+	const struct arch_syscall_def *table = riscv64_syscall_table;
 
 	/* XXX - plenty of room for future improvement here */
 	for (iter = 0; table[iter].name != NULL; iter++) {
@@ -498,10 +498,10 @@ int aarch64_syscall_resolve_name(const char *name)
  * syscall names; returns NULL on failure.
  *
  */
-const char *aarch64_syscall_resolve_num(int num)
+const char *riscv64_syscall_resolve_num(int num)
 {
 	unsigned int iter;
-	const struct arch_syscall_def *table = aarch64_syscall_table;
+	const struct arch_syscall_def *table = riscv64_syscall_table;
 
 	/* XXX - plenty of room for future improvement here */
 	for (iter = 0; table[iter].num != __NR_SCMP_ERROR; iter++) {
@@ -521,8 +521,8 @@ const char *aarch64_syscall_resolve_num(int num)
  * function should only ever be used internally by libseccomp.
  *
  */
-const struct arch_syscall_def *aarch64_syscall_iterate(unsigned int spot)
+const struct arch_syscall_def *riscv64_syscall_iterate(unsigned int spot)
 {
 	/* XXX - no safety checks here */
-	return &aarch64_syscall_table[spot];
+	return &riscv64_syscall_table[spot];
 }
--- src/arch.c
+++ src/arch.c
@@ -41,6 +41,7 @@
 #include "arch-parisc.h"
 #include "arch-ppc.h"
 #include "arch-ppc64.h"
+#include "arch-riscv64.h"
 #include "arch-s390.h"
 #include "arch-s390x.h"
 #include "db.h"
@@ -94,6 +95,8 @@ const struct arch_def *arch_def_native = &arch_def_ppc;
 const struct arch_def *arch_def_native = &arch_def_s390x;
 #elif __s390__
 const struct arch_def *arch_def_native = &arch_def_s390;
+#elif __riscv && __riscv_xlen == 64
+const struct arch_def *arch_def_native = &arch_def_riscv64;
 #else
 #error the arch code needs to know about your machine type
 #endif /* machine type guess */
@@ -152,6 +155,8 @@ const struct arch_def *arch_def_lookup(uint32_t token)
 		return &arch_def_ppc64;
 	case SCMP_ARCH_PPC64LE:
 		return &arch_def_ppc64le;
+	case SCMP_ARCH_RISCV64:
+		return &arch_def_riscv64;
 	case SCMP_ARCH_S390:
 		return &arch_def_s390;
 	case SCMP_ARCH_S390X:
@@ -202,6 +207,8 @@ const struct arch_def *arch_def_lookup_name(const char *arch_name)
 		return &arch_def_ppc64;
 	else if (strcmp(arch_name, "ppc64le") == 0)
 		return &arch_def_ppc64le;
+	else if (strcmp(arch_name, "riscv64") == 0)
+		return &arch_def_riscv64;
 	else if (strcmp(arch_name, "s390") == 0)
 		return &arch_def_s390;
 	else if (strcmp(arch_name, "s390x") == 0)
